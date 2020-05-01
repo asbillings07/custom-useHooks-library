127d25464127a7e88781ea9425f9815d240894bb
asbillings07 <superswiftdeveloper@gmail.com>
2020-05-01 16:19:36 -0400

    update useInc hook with step

--- src/hooks/useIncrement.js
+++ src/hooks/useIncrement.js
@@ -2,14 +2,30 @@ import { useState } from 'react'
 
 // Use Increment Hook can be used for all sorts of things!!
 
-export const useInc = ({ maxValue = 10000, initialValue = 0, minValue = -10000 }) => {
+export const useInc = ({ maxValue = 10000, initialValue = 0, minValue = -10000, step = 0 }) => {
   const [value, setValue] = useState(initialValue)
 
   const inc = () => {
-    setValue((prevState) => (prevState >= maxValue ? maxValue : prevState + 1))
+    setValue((prevState) => {
+      if (step > 0 && prevState + step >= maxValue) {
+        return maxValue
+      } else if (step <= 0) {
+        return prevState + 1
+      } else {
+        return prevState + step
+      }
+    })
   }
   const dec = () => {
-    setValue((prevState) => (prevState <= minValue ? minValue : prevState - 1))
+    setValue((prevState) => {
+      if (step > 0 && prevState - step <= minValue) {
+        return minValue
+      } else if (step <= 0) {
+        return prevState - 1
+      } else {
+        return prevState - step
+      }
+    })
   }
 
   const reset = () => setValue(initialValue)
