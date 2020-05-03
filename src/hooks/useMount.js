87d199f452987015ee455401a864f57d386a65cf
asbillings07 <superswiftdeveloper@gmail.com>
2020-05-03 19:26:24 -0400

    update comments

--- src/hooks/useMount.js
+++ src/hooks/useMount.js
@@ -1,6 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from 'react'
 
+// enables you to run functions in useEffect in class components.
 export const useMount = (fn) => {
   useEffect(() => {
     fn()
