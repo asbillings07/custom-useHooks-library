9c34f6eb9349c2affe047edd8e9a6c4719661c5f
asbillings07 <superswiftdeveloper@gmail.com>
2020-05-11 13:27:59 -0400

    add useMeasure, useScrollfreeze and useWindowWidth hooks

--- src/components/Hover.js
+++ src/components/Hover.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { Card } from '../Elements'
 import black from '../black.png'
-import { useHover } from '../hooks'
+import { useHover, useWindowWidth, useMeasure } from '../hooks'
 
 const Hover = () => {
   const { bind, isHovered } = useHover()
+  const width = useWindowWidth()
+  const [{ ref }, bounds] = useMeasure()
+
+  console.log('bounds', bounds)
+
+  if (width <= 400) return null
 
   return (
     <div>
-      <Card {...bind} style={{ background: isHovered ? 'var(--purp)' : 'var(--black)' }}>
+      <Card ref={ref} {...bind} style={{ background: isHovered ? 'var(--purp)' : 'var(--black)' }}>
         <h3>Some card</h3>
         <img src={black} alt='' />
       </Card>
